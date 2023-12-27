99d5592cc6890602faf5b9167278fe1b47592681
Haruki Uchiito <uhcahriuiktio0114@outlook.com>
2023-12-27 21:39:54 +0900

    add WorldFrame definition

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,50 @@ pub mod grpc_fs {
 pub mod grpc_data_transfer {
     tonic::include_proto!("grpc_data_transfer");
 }
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct WheelVelocity {
+    value: f64,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Imu {
+    angular_rates: Option<[f64; 3]>,
+    accelerations: Option<[f64; 3]>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Pose2D {
+    position: Point2D,
+    theta: f64,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub enum Measurement {
+    WheelVelocity(WheelVelocity),
+    IMU(Imu),
+    Pose2D(Pose2D),
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub enum Estimate {
+    Pose2D(Pose2D),
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Entity {
+    measurement_map: std::collections::HashMap<String, Measurement>,
+    estimate_map: std::collections::HashMap<String, Estimate>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct WorldFrame {
+    timestamp: f64, // unix time in sec
+    entity_map: std::collections::HashMap<String, Entity>,
+}
